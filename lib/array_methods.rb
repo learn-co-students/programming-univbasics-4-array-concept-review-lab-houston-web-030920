c641606dcd0c57914e4f2ed14465cba1c5c05f06
Timothy Morales <timothydanielmorales@gmail.com>
2020-02-02 00:12:46 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,15 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  i = value_to_find
+  array.include?(i)
+  return array.index(i)
 end
 
 def find_max_value(array)
-  # Add your solution here
+  i = array.max
+  return i
 end
 
 def find_min_value(array)
-  # Add your solution here
+  i = array.min
+  return i 
 end
