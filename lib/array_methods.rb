743fc810013ee6b95130757e4c9eea4a1416be65
Bryant Omoregie <unclebryant@gmail.com>
2020-02-12 01:34:40 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,9 +9,43 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  array.max
+  x = array[0] 
+  array.length.times do |y|
+    if x < array[y] 
+      x = array[y]
+    end
+  end
+    return x
 end
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def find_min_value(array)
-  array.min
+  x = array[0] 
+  array.length.times do |y|
+    if x > array[y]
+      x = array[y]
+    end  
+  end  
+  return x
 end
